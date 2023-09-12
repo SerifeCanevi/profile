be988a234a6d224312bd79a9be39c36aae6c3786
şerife canevi <93770226+SerifeCanevi@users.noreply.github.com>
2023-09-12 18:28:20 +0300

    Update AccountMapper.java

--- src/main/java/com/canevi/profile/domain/mapper/AccountMapper.java
+++ src/main/java/com/canevi/profile/domain/mapper/AccountMapper.java
@@ -1,5 +1,6 @@
 package com.canevi.profile.domain.mapper;
 
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -7,13 +8,19 @@ import com.canevi.profile.dao.model.Account;
 import com.canevi.profile.domain.dto.AccountDTO;
 import com.canevi.profile.domain.request.AccountCreateRequest;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD)
 public interface AccountMapper {
 
     @Mapping(target = "accountId", source = "id")
+    @Mapping(target = "username", source = "username")
+    @Mapping(target = "password", source = "password")
+    @Mapping(target = "email", source = "email")
     AccountDTO genereateAccountDTOFromAccount(Account account);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "username", source = "username")
+    @Mapping(target = "password", source = "password")
+    @Mapping(target = "email", source = "email")
     Account genereateAccountFromAccountCreateRequest(AccountCreateRequest request);
     
 }
